0c36b3cd18dc225195195ddd4334de3d9c12088c
Neville <neville.code@gmail.com>
2020-07-29 13:54:24 +1200

    Initialize ML model

--- ImagesClassify.py
+++ ImagesClassify.py
@@ -24,9 +24,34 @@ train_images = train_images/255.0
 test_images = test_images/255.0
 
 
-print(train_labels[0])
-print(train_images[7])
+# print(train_labels[0])
+# print(train_images[7])
 
 # Using matplot to plot the image 
-plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+# plt.imshow(train_images[7], cmap=plt.cm.binary)
+# plt.show()
+
+
+
+## Creating a model
+
+# softmax function makes it such that all the output add up to 1
+# This is done to minmic probability.
+
+model =keras.Sequential([
+	keras.layers.Flatten(input_shape=(28,28)),
+	keras.layers.Dense(128, activation="relu"),
+	keras.layers.Dense(10, activation="softmax")
+	])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+# epochs determined the number of the same images show to the model
+# The order of the input might influence the parameter of the network.
+# This is done to increase the accracy of the model.
+model.fit(train_images, train_labels, epochs=10)
+
+
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print("Tested Acc: ", test_acc)
