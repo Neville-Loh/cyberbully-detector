bd40399bba4f0d32be1ad0f43fc2595863a5c34b
Neville <neville.code@gmail.com>
2020-07-30 12:35:44 +1200

    added model predictions

--- text_classification/Text_classify.py
+++ text_classification/Text_classify.py
@@ -7,7 +7,7 @@ data = keras.datasets.imdb
 
 # only take the most frequence 10k words
 # The words are represented in integer
-(train_data, tran_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
 
 #print(train_data[0])
 
@@ -45,7 +45,7 @@ def decode_review(text):
 
 model = keras.Sequential()
 model.add(keras.layers.Embedding(10000, 16))
-model.add(keras.layers.GlobalAveragePolling1D())
+model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))     # configure output neuron to become sigmodial between 0, 1, indicating the movie is good or bad.
 
@@ -58,12 +58,29 @@ model.summary()
 model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
 
-
 # spliting training data into 2 set
 
 x_val = train_data[:10000]
 x_train = train_data[10000:]
 
 # valadation data
-x_val = train_data[:10000]
-x_train = train_data[10000:]
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+# the batch_size 
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val,y_val), verbose=1)
+
+results = model.evaluate(test_data, test_labels)
+
+
+# showing train result
+print(results)
+
+test_review = test_data[0]
+predict = model.predict([test_review])
+print("Review: ")
+print(decode_review(test_review))
+print("Prediction: " + str(predict[0]))
+print("Actual: " + str(test_labels[0]))
+print(results)
+
